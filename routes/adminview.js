7bdd9b3c633de3a30d85fc6a0e2deefe7438fd48
KGordon <krisan_gordon@yahoo.com>
2022-05-19 23:06:08 -0500

    optimized login and logout session

--- routes/adminview.js
+++ routes/adminview.js
@@ -20,4 +20,62 @@ router.get('/admin', function(req, res, next) {
     }
 });
 
+
+
+router.get('/viewOrders/edit/:id', function(req, res) { //must be router.get or app.get or whatever else i choose but it has to be a get http verb
+    conn.query('SELECT * FROM customer_orders WHERE id=' + req.params.id, function(err,row){
+        if(err) {
+            console.log('not being rendered');
+        } else {
+            res.render('../views/editOrders', {cust_orders:row});
+        }
+    });
+});
+
+   /* POST ordersedit page. */
+router.post('/viewOrders/update', function(req, res, next) {
+      
+    let sqlQuery = "UPDATE customer_orders SET customer_order ='" + req.body.cust_order +  
+                                        "', customer_email ='" +  req.body.cust_email +
+                                        "', order_date ='" +  req.body.order_date + 
+                                        "' WHERE id = " + req.body.id;
+
+    conn.query(sqlQuery, function(err,rows)     {
+
+        if(err) {
+            console.log(err)
+        } else {
+            res.redirect('/admin');   
+            next();
+        }   
+               //req.flash('error', err); 
+                              
+            });
+           
+       });
+
+
+
+
+  /* GET ORDERS DELETE METHOD. */
+  router.get('/viewOrders/delete/:id', function(req, res, next) {
+      
+    conn.query('DELETE FROM customer_orders WHERE id='+ req.params.id, function(err,row)     {
+    
+           if(err){
+            //    req.flash('error', err); 
+            console.log(err)
+              
+           }else{
+            // req.flash('success', 'Deleted successfully!!'); 
+            res.redirect('/admin');   
+            next();      
+           }
+                               
+            });
+           
+       });
+
+       
+
 module.exports = router;
